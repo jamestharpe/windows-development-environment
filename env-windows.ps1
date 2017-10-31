7754c1e442efca097133ecbd7ffb1149bb41bc91
James Tharpe <jimmy.tharpe@gmail.com>
2017-10-31 16:14:08 -0400

    Adds plantuml vscode extension

--- env-windows.ps1
+++ env-windows.ps1
@@ -138,6 +138,9 @@ npm install -g react-native-cli
 # Docker support
 code --install-extension PeterJausovec.vscode-docker
 
+# PlantUML support
+code --install-extension jebbs.plantuml
+
 #
 # MySQL
 #
