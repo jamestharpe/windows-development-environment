fb855e41952092f09fcfddf71017efd86d0dd24e
James Tharpe <jimmy.tharpe@gmail.com>
2018-03-21 16:53:03 -0400

    Adds git aliases

--- env-windows.ps1
+++ env-windows.ps1
@@ -33,6 +33,12 @@ Update-Environment-Path
 choco install git --yes
 choco install tortoisegit --yes
 Update-Environment-Path
+git config --global alias.pom 'pull origin master'
+git config --global alias.last 'log -1 HEAD'
+git config --global alias.ls "log --pretty=format:'%C(yellow)%h %ad%Cred%d %Creset%s%Cblue [%cn]' --decorate --date=short"
+git config --global alias.ammend "commit -a --amend"
+git config --global alias.standup "log --since yesterday --author $(git config user.email) --pretty=short"
+git config --global alias.everything "!git pull && git submodule update --init --recursive"
 
 #
 # AWS awscli
