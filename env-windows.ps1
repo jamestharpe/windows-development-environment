834440987eaacc51967adc0245caa7cd059d8ac3
James Tharpe <jimmy.tharpe@gmail.com>
2018-04-19 17:22:05 -0400

    Adds Firacode font
    See https://www.youtube.com/watch?v=KI6m_B1f8jc

--- env-windows.ps1
+++ env-windows.ps1
@@ -212,6 +212,7 @@ choco install firefox --yes
 choco install sysinternals --yes
 choco install procexp --yes
 choco install awscli --yes
+choco install firacode --yes # See https://www.youtube.com/watch?v=KI6m_B1f8jc
 
 Update-Environment-Path
 
