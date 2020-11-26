e1eb43e015e1bec8915db4a5c1d83b21b9cb9ab2
James Tharpe <jimmy.tharpe@gmail.com>
2020-11-26 07:03:58 -0500

    Update env-windows.ps1
    Rename master to main

--- env-windows.ps1
+++ env-windows.ps1
@@ -42,7 +42,7 @@ Get-Command -Module Microsoft.PowerShell.Archive
 choco install git --yes --params '/GitAndUnixToolsOnPath'
 choco install tortoisegit --yes
 Update-Environment-Path
-git config --global alias.pom 'pull origin master'
+git config --global alias.pom 'pull origin main'
 git config --global alias.last 'log -1 HEAD'
 git config --global alias.ls "log --pretty=format:'%C(yellow)%h %ad%Cred%d %Creset%s%Cblue [%cn]' --decorate --date=short"
 git config --global alias.ammend "commit -a --amend"
