a5de58a305a135ebc8a597fcda580cd6bba3f207
James Tharpe <jimmy.tharpe@gmail.com>
2021-12-09 14:49:18 -0500

    Latest choco install script

--- env-windows.ps1
+++ env-windows.ps1
@@ -28,8 +28,7 @@ function Push-User-Path($userPath) {
 #
 
 # Choco
-if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) { Start-Process powershell.exe "-NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`"" -Verb RunAs; exit }
-Invoke-WebRequest https://chocolatey.org/install.ps1 -UseBasicParsing | Invoke-Expression
+Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
 Update-Environment-Path
 
 # Utils
